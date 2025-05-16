3cbaf308881b99273c36d83fd07f544c424527b5
username <abc@xyz>
2025-05-16 16:50:14 +0200

    Fixed inefficiencies

--- Covauto.Shared/DTO/Gebruiker/GebruikerItem.cs
+++ Covauto.Shared/DTO/Gebruiker/GebruikerItem.cs
@@ -1,9 +1,6 @@
 namespace Covauto.Shared.DTO.Gebruiker;
 
-public class GebruikerItem
+public class GebruikerItem : GebruikerListItem
 {
-    public int ID { get; set; }
-    public string Voornaam { get; set; }
-    public string Achternaam { get; set; }
     public bool Admin { get; set; }
 }
--- Covauto.Shared/DTO/Rit/RitItem.cs
+++ Covauto.Shared/DTO/Rit/RitItem.cs
@@ -3,11 +3,7 @@ using Covauto.Shared.DTO.Gebruiker;
 
 namespace Covauto.Shared.DTO.Rit;
 
-public class RitItem
+public class RitItem : RitListItem
 {
-    public int ID { get; set; }
     public List<AdresListItem>? Adressen { get; set; }
-    public GebruikerListItem Gebruiker { get; set; }
-    public int Kilometers { get; set; }
-    public DateTime Datum { get; set; }
 }
