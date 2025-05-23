7b194675a134498fc01ec734a46038bc4d2ff1c8
Metehan <metehan@exapps.nl>
2025-05-23 11:05:02 +0200

    Ritten overzicht

--- Covauto.Blazor/Pages/Ritten/Overzicht/RittenOverzicht.razor.cs
+++ Covauto.Blazor/Pages/Ritten/Overzicht/RittenOverzicht.razor.cs
@@ -1,7 +1,21 @@
+using Covauto.Shared.DTO.Rit;
 using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace Covauto.Blazor.Pages.Ritten.Overzicht;
 
 public partial class RittenOverzicht : ComponentBase
 {
+    [Inject]
+    private HttpClient HttpClient { get; set; }
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; }
+
+    private IEnumerable<RitListItem> ritten = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        ritten = await HttpClient.GetFromJsonAsync<IEnumerable<RitListItem>>("api/Rit");
+    }
 }
