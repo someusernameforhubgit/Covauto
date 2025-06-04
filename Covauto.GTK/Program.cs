d79f73e480a7a6a700aa78cea14d2f9c7a79c5a3
username <abc@xyz>
2025-06-04 11:56:08 +0200

    Commit

---
+++ Covauto.GTK/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CovAuto.GTK;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var application = Gtk.Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
+        application.OnActivate += (sender, args) =>
+        {
+            var window = Gtk.ApplicationWindow.New((Gtk.Application) sender);
+            window.Title = "Gtk4 Window";
+            window.SetDefaultSize(300, 300);
+            window.Show();
+        };
+        application.RunWithSynchronizationContext(null);
+    }
+}
