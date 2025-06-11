60277cbb420b5a2744a5d6d5b87a47079d9e22ba
Username <167310058+someusernameforhubgit@users.noreply.github.com>
2025-06-11 09:46:37 +0000

    Init (#21)
    Co-authored-by: username <abc@xyz>

--- Covauto.GTK/MainWindow.cs
+++ Covauto.GTK/MainWindow.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Covauto.Shared.DTO.Auto;
+using Covauto.Shared.DTO.Rit;
 using System.IO;
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
@@ -10,15 +16,90 @@ namespace Covauto.GTK
     {
         [UI] private Button Export = null;
 
+        private static HttpClientHandler handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+        };
+        private static readonly HttpClient client = new HttpClient(handler);
+        private TreeView treeView;
         public MainWindow() : this(new Builder("MainWindow.glade")) { }
 
         private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
         {
             builder.Autoconnect(this);
-
+            
+            treeView = builder.GetObject("Treeview") as TreeView;
+            
+            var treeStore = new TreeStore(
+                typeof(int),
+                typeof(string),
+                typeof(string),
+                typeof(int),
+                typeof(string),
+                typeof(string)
+            );
+            treeView.Model = treeStore;
+            
+            PrepareTreeView();
+            
+            Shown += ShownHandler;
             DeleteEvent += Window_DeleteEvent;
 
             Export.Clicked += OnExportButtonClicked;
+        }
+        
+        private void PrepareTreeView()
+        {
+            var idCell = new CellRendererText();
+            var idColumn = new TreeViewColumn("ID", idCell, "text", 0);
+            treeView.AppendColumn(idColumn);
+            
+            var gebruikerCell = new CellRendererText();
+            var gebruikerColumn = new TreeViewColumn("Gebruiker", gebruikerCell, "text", 1);
+            treeView.AppendColumn(gebruikerColumn);
+            
+            var autoCell = new CellRendererText();
+            var autoColumn = new TreeViewColumn("Auto", autoCell, "text", 2);
+            treeView.AppendColumn(autoColumn);
+            
+            var kmCell = new CellRendererText();
+            var kmColumn = new TreeViewColumn("Kilometers", kmCell, "text", 3);
+            treeView.AppendColumn(kmColumn);
+            
+            var beginCell = new CellRendererText();
+            var beginColumn = new TreeViewColumn("Begin", beginCell, "text", 4);
+            treeView.AppendColumn(beginColumn);
+            
+            var endCell = new CellRendererText();
+            var endColumn = new TreeViewColumn("End", endCell, "text", 5);
+            treeView.AppendColumn(endColumn);
+            
+            foreach (TreeViewColumn col in treeView.Columns)
+            {
+                col.Resizable = true;
+            }
+        }
+        
+        private async void ShownHandler(object sender, EventArgs e)
+        {
+            var response = await client.GetFromJsonAsync<List<RitListItem>>("https://localhost:7221/api/Rit");
+            var treeStore = treeView.Model as TreeStore;
+            
+            var autoResponse = await client.GetFromJsonAsync<List<AutoListItem>>("https://localhost:7221/api/Auto");
+            var autos = autoResponse.ToDictionary(auto => auto.ID, auto => auto);
+            
+            foreach (var partialRit in response)
+            {
+                var rit = await client.GetFromJsonAsync<RitItem>("https://localhost:7221/api/Rit/" + partialRit.ID);
+                treeStore.AppendValues(
+                    rit.ID,
+                    rit.Gebruiker.Voornaam + " " + rit.Gebruiker.Achternaam,
+                    autos[rit.AutoId].Merk + " " + autos[rit.AutoId].Model,
+                    rit.Kilometers,
+                    rit.Begin.ToString("yyyy-MM-dd  HH:mm:ss"),
+                    rit.End.ToString("yyyy-MM-dd  HH:mm:ss")
+                );
+            }
         }
 
         private void Window_DeleteEvent(object sender, DeleteEventArgs a)
