2dc19b171603b877bd9735635b273371f0ac40e8
username <abc@xyz>
2025-06-13 11:29:03 +0200

    UI changes

--- Covauto.Blazor/Pages/Ritten/Bekijken/RitBekijken.razor.cs
+++ Covauto.Blazor/Pages/Ritten/Bekijken/RitBekijken.razor.cs
@@ -2,6 +2,8 @@ using Covauto.Shared.DTO.Rit;
 using Microsoft.AspNetCore.Components;
 using System.Net.Http.Json;
 using Covauto.Shared.DTO.Adres;
+using Covauto.Shared.DTO.Auto;
+using Covauto.Shared.DTO.Gebruiker;
 
 namespace Covauto.Blazor.Pages.Ritten.Bekijken;
 
@@ -17,6 +19,8 @@ public partial class RitBekijken : ComponentBase
     private NavigationManager NavigationManager { get; set; }
 
     private RitItem rit;
+    private AutoItem auto;
+    private GebruikerItem gebruiker;
     private IEnumerable<AdresListItem> adressen = [];
 
     protected override async Task OnInitializedAsync()
@@ -31,6 +35,9 @@ public partial class RitBekijken : ComponentBase
             GebruikerId = result.GebruikerId,
             Adressen = result.Adressen,
         };
+        
+        auto = await HttpClient.GetFromJsonAsync<AutoItem>($"api/Auto/{rit.AutoId}");
+        gebruiker = await HttpClient.GetFromJsonAsync<GebruikerItem>($"api/Gebruiker/{rit.GebruikerId}");
     }
     
     private void RitAanmaken()
